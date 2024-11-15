ee1617c2ba863edc8728f0116691fa2b2ddad40c
shailendrathakur300 <shailendrathakuracademic123@gmail.com>
2024-11-15 19:32:24 +0530

    remove wave component and try to fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import { WavyBackground } from '@/components/ui/wavy-background'
 import EarthquakePrediction from './_component/earthquake-prediction'
 
 export default function Home() {
-  return (
-    <WavyBackground className="max-w-4xl mx-auto pb-40">
-      <EarthquakePrediction />
-    </WavyBackground>
-  )
+  return <EarthquakePrediction />
 }
